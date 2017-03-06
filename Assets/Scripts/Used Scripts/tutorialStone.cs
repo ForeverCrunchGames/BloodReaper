057344bb93c3f742e5cca1f50de2eec7033e1119
Aleix Rafegas Farré <aleix.rafegas.farre@gmail.com>
2017-03-06 21:39:24 +0100

    Added functional tutorialstone prefab

---
+++ Assets/Scripts/Used Scripts/tutorialStone.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialStone : MonoBehaviour {
+
+	//EXPLANATION: Detects if "player" enters the collider, inits an animation and show a gameobject, the same if the player exits the collider.
+
+    private Animator animator;
+    private bool isPlayerIn; 
+    private int state;
+
+    void Start () 
+    {
+        animator = GetComponent<Animator>();
+        state = 0;
+	}
+	
+	void Update () 
+    {
+        if (isPlayerIn)
+        {
+            if (state == 1)
+            {
+                animator.SetTrigger("open");
+                state = 0;
+            }
+        }
+        else
+        {
+            if (state == 1)
+            {
+                animator.SetTrigger("close");
+                state = 0;
+            }
+        }
+	}
+
+
+    //Triggers
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isPlayerIn = true;
+            state = 1;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isPlayerIn = false;
+            state = 1;
+
+        }
+    }
+}
