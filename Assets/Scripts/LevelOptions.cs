58963e11016985bf9cf5c758dd4c6660fa4c8b5c
Aleix Rafegas Farré <aleix.rafegas.farre@gmail.com>
2017-05-02 23:30:48 +0200

    lvl 1.1 initial decoration

--- Assets/Scripts/LevelOptions.cs
+++ Assets/Scripts/LevelOptions.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelOptions : MonoBehaviour {
+public class LevelOptions : MonoBehaviour 
+{
+    PlayerMOD player;
+
+    [Header("Player Options")]
+    public bool isSword;
+    public bool isWallSlide;
+    public bool isAngularSlide;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMOD>();
+
+        if (isSword == true)
+        {
+            player.isPlayerOverpowered = true;
+            player.isLifeDecreasing = true;
+        }
+
+        if (isWallSlide == true)
+        {
+            player.isPlayerHaveWallSlide = true;
+        }
+
+        if (isAngularSlide == true)
+        {
+            player.isPlayerHaveAngularSlide = true;
+        }
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 		
 	}
 }
