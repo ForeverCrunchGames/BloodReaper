f12168e73aa1678aa29c75c333d806d53be1f838
Aleix Rafegas Farré <aleix.rafegas.farre@gmail.com>
2017-05-29 18:41:36 +0200

    Controller basic inputs

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMenu : MonoBehaviour {
+
+    LevelLogic levelLogic;
+
+	// Use this for initialization
+	void Start () 
+    {
+        levelLogic = GameObject.FindGameObjectWithTag("LevelMnanager").GetComponent<LevelLogic>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	}
+
+    public void GoMenu()
+    {
+        levelLogic.LoadScene(1);
+    }
+}
