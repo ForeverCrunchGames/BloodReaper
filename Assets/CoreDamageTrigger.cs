e7dd06ab537e47a334639ba6a5a65b992d9e1038
Aleix Rafegas Farré <aleix.rafegas.farre@gmail.com>
2017-06-02 08:45:44 +0200

    1.3 blocking progress

---
+++ Assets/CoreDamageTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreDamageTrigger : MonoBehaviour {
+
+    public CoreLogic coreLogic;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (coreLogic.isDestroyed)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Attack")
+        {
+            coreLogic.Damage();
+        }
+    }
+}
