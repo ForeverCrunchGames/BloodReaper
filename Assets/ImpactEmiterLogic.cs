0c042bb7674737ac32a3794816ecab80f3f4e194
Aleix Rafegas Farré <aleix.rafegas.farre@gmail.com>
2017-06-07 03:11:59 +0200

    mega update lvl 1.3

---
+++ Assets/ImpactEmiterLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactEmiterLogic : MonoBehaviour {
+
+    public GameObject DustParticleSystem;
+    public AudioSource impactSound;
+    //public float frequency;
+    //public float initDelay;
+
+    //float counter;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //counter = counter + initDelay;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+//        counter += Time.deltaTime;
+//
+//        if (counter > frequency)
+//        {
+//            counter = 0;
+//            impactSound.Play();
+//            Instantiate(DustParticleSystem, this.transform.position, this.transform.rotation);
+//        }
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Hazzard")
+        {
+            impactSound.Play();
+            Instantiate(DustParticleSystem, this.transform.position, this.transform.rotation);
+        }
+        //}
+    }
+}
